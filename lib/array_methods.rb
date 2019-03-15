e13139d9eb1f7f4ec0cf04d8d1fb4e829f856ed7
Xabier C <xabier_abia@hotmail.com>
2019-03-15 01:10:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def using_include(array, element)
-   array.include?("element")
+  array.include?("element")
   end
 
 def using_sort(array)
@@ -11,11 +11,11 @@ def using_reverse(array)
 end
 
 def using_first(array)
-  array
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
